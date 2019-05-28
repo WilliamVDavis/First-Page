4d21dbd9ad7248b32f34b26a64138099196a0768
WilliamVDavis <50843184+WilliamVDavis@users.noreply.github.com>
2019-05-28 16:45:14 -0500

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,24 @@
+// Question 1
+function largestSwap(num) {
+var str = 'num';
+if ('num' > reverseString('num') {
+    'num' = true;
+}
+return largestSwap
+}
+
+// Question 2
+function isSpecialArray(arr) {
+i = arr;
+if (i % 1, i % 0); {
+    return (true)}
+    else (i % 0, i % 0 || i % 1, i % 1); {
+        return (fasle)
+    }
+}
+}
+
+//Question 3
+function formatNum(num) {
+    return(num.toLocalString());
+}
