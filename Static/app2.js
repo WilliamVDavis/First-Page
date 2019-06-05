4a2e4c851b5638cb4a25e865a3f1daaed5f3ddb2
WilliamVDavis <50843184+WilliamVDavis@users.noreply.github.com>
2019-06-05 09:18:25 -0500

    Add files via upload

---
+++ Static/app2.js
@@ -0,0 +1,29 @@
+const jacks = document.querySelectorAll(".jack")
+
+jacks.forEach(function(jack) { // For the array jacks it's saying where there is a jack insert funcion  
+  jack.addEventListener("click", function(d) {// For any box named jack listen for a click
+    jack.innerHTML = "X";// add an x
+
+jacks.forEach(function() {
+    jack.addEventListener("click", function(d) {
+      jack.innerHTML = "O";
+
+      jacks.forEach(function(O) {
+        jack.addEventListener("click", function(d) {
+          jack.innerHTML = "X";
+
+          jacks.forEach(function(X) {
+            jack.addEventListener("click", function(d) {
+              jack.innerHTML = "O";
+            })
+          });   
+        })
+      });
+    })
+  });
+})
+});
+
+function restart() {
+    document.location.href="";
+}
