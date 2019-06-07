c4dc472e58c53e7c28a3052db3d81b22d61c21cf
WilliamVDavis <50843184+WilliamVDavis@users.noreply.github.com>
2019-06-07 16:46:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const express = require('express');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+// properties...
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'Boondocks'
+});
+
+connection.connect(function(error) {
+    if(error) {
+        console.log('Error');
+    } else {
+        console.log('Connected');
+    }
+});
+
+app.get('/', function(req, resp) {
+    // about mysql
+    connection.query("SELECT * FROM Boondocks", function(error, rows, fields))
+    //callback
+    if (error) {
+        console.log('Error in the query');
+    } else {
+        console.log('Sucessful query');
+        //parse with your rows/fields
+    }
+});
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000 
